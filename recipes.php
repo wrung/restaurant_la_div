98db3a4386b0d53cbe4b88677edad0db9fcc9071
wrung <contact@pascalprospa.com>
2017-03-10 15:27:31 -0400

    test commit

---
+++ recipes.php
@@ -0,0 +1,150 @@
+<!DOCTYPE>
+
+<html lang="fr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Liste des recettes</title>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="GIT/restaurant_la_div/recipes/css/style.css">
+	 
+<style>
+body{
+	background-color: #483932 ;
+}
+header{
+	
+	background-color: #B58263;
+	background-repeat: no-repeat;
+	text-align: center;
+}
+ #article{
+ 	
+ 	width: 50px;
+ 	height: 50px;
+ 	float: left;
+
+ 	
+ }
+ #aside{
+ 	width: 400px;
+ 	height: 50px;
+ 	font-size: 30px;
+ 	float: right;
+ 	background-color: #B38367;
+ 	color: #E0D9D3;
+ 	f
+ }
+ h1{
+ 	color: #E0D9D3;
+ }
+ 
+</style>
+</head>
+<body>
+<header>
+<h1>Les nouvelles recettes du chef<h1>
+<h3><a href="index.php">Retour accueil</a><h3>
+</header>
+
+<section class="container"> 
+<div id="article" ><img src="assets/img/salade.jpg" alt="recette 1"></div>
+<div id="aside">
+<p><h6>
+Temps de préparation : 30 minutes
+Temps de cuisson : 15 minutes
+
+Ingrédients (pour 4 personnes) :
+- 500 g de spaghettini (numéro 3)
+- 1 oignon rouge
+- 1/2 poivron jaune
+- 1/2 poivron rouge
+- 1/2 aubergine
+- 3 courgettes (petites)
+- 1 carotte
+- piment
+- huile d’olive
+- sel
+- parmesan râpé
+Couper les légumes en petits morceaux. Dans une poêle, faire revenir les légumes dans l’huile bien chaude.
+Laisser rissoler en mélangeant régulièrement jusqu’à ce qu’ils soient légèrement dorés. Saler et ajouter une pincée de piment.
+Cuire à part les spaghettini et bien mélanger avec les légumes avant de servir. Saupoudrer de parmesan.<h6></p></div>
+
+
+
+<div id="article" ><img src="assets/img/spaguettis.jpg" alt="recette 2"></div>
+<div id="aside">
+<p><h6>Temps de préparation : 5 minutes
+Temps de cuisson : 15 minutes
+
+Ingrédients (pour 4 Personnes) :
+- 4 pavés de saumon
+- 3 échalotes
+- 20 cl de soja cuisine 
+
+Préparation de la recette :
+
+Allumer le four à 180°C, déposer le saumon dans un plat allant au four, éplucher 3 grosses échalotes (cuisses de poulet).
+Étaler la crème sur le poisson, parsemer les échalotes dessus, saler, mettre au four. Quand les échalotes brunissent le poisson est cuit, servir avec riz, pommes vapeur. 
+En cas de poisson congelé mettre le four à 150°C. Cuisson un peu plus longue pour assurer la décongélation du poisson.
+Boisson conseillée :<h6></p></div>
+<div id="" ="article"> <img src="assets/img/viandegrillée.jpg" alt="recette 3"></div>
+<div id="aside"><h6>
+<p>Temps de préparation : 5 minutes
+Temps de cuisson : 30 minutes
+
+Ingrédients (pour 6 personnes) :
+- 2 aubergines
+- 4 courgettes longues (moyennes)
+- 1 poivron rouge (ou jaune)
+- 2 gousses d'ail
+- 10 cl d'huile d'olive fruitée
+- 3 cuillères à soupe de vinaigre balsamique
+- 1 pincée de sel fin (pas obligatoire)
+
+Préparation de la recette :
+
+Passer les gousses d'ail au presse ail, ou les écraser en purée.
+les ajouter à l'huile d'olive dans un bol.
+Ajouter le vinaigre balsamique, et réserver.
+Détailler les aubergines en fines tranches (2 à 3 mm).
+Détailler les courgettes de la même façon, mais si possible en tranches plus fines que l'aubergine (avec un économe, c'est l'idéal).
+Découper les poivrons en quatre, et les débarasser de leurs graines et parties blanches.<h6></p>
+</div>
+</div>
+</section>
+
+<footer>
+	
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
