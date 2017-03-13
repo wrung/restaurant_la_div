aa2666319a91ea12526c93a8056f986767e28fab
mjrv <hervejurin972@gmail.com>
2017-03-13 13:53:05 -0400

    quelque avancees
    Index.php accueill de morgan
    index_formulaire_contact.php --- pour le formulaire
    recipes.php --- pour entrer une recette
    update_recipes.php ----- pour maj recette
    list_recipes --- pour liste des recettes

---
+++ list_recipes.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'inc/connect.php';
+
+// On selectionne les colonnes id & title de la table recettes
+$select = $bdd->prepare('SELECT id,title FROM recipes ORDER BY id DESC');
+if($select->execute()){
+	$recettes = $select->fetchAll(PDO::FETCH_ASSOC);
+}
+else {
+	// Erreur de développement
+	var_dump($select->errorInfo());
+	die; // alias de exit(); => die('Hello world');
+}
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Liste de recettes</title>
+</head>
+<body>
+	<h1>Les recettes existantes</h1>
+
+	<br>
+	<table>
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>titre</th>
+				<th>détails</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<!-- foreach permettant d'avoir une ligne <tr> par ligne SQL -->
+			<?php foreach($recettes as $recette): ?>
+				<tr>
+					<td><?=$recette['id']; ?></td>
+					<td><?=$recette['title']; ?></td>
+					<td>
+						<!-- view_menu.php?id=6 -->
+						<a href="view_menu.php?id=<?=$recette['id']; ?>">
+							Visualiser
+						</a>
+					</td>
+					<td>
+						<!-- view_menu.php?id=6 -->
+						<a href="update_recipes.php?id=<?=$recette['id']; ?>">
+							Update
+						</a>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+</body>
+</html>
