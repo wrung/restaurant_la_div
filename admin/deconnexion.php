dc4af380c7b929257706522caeda2b9b1c3b418f
Nico <nico97280@hotmail.com>
2017-03-14 11:18:27 -0400

    Déconnexion

---
+++ admin/deconnexion.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Deconnexion</title>
+</head>
+<body>
+	<h1>Confirmer la déconnexion?</h1>
+	<a href="deco.php"><input type="button" value="Déconnexion"></a>
+	<!--<a href="listerecette.php"><input type="button" value="Non"></a>-->
+</body>
+</html>
